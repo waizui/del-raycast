d70669b2c842cec1803b5473f84693b567569e73
Mason Ma <majq@g.ecc.u-tokyo.ac.jp>
2024-11-12 18:27:07 +0900

    rasterization for cornell box

--- examples/03_material_test_ball.rs
+++ examples/03_material_test_ball.rs
@@ -17,7 +17,7 @@ fn parse() -> anyhow::Result<(Vec<Shape>, f32, [f32; 16], (usize, usize))> {
     let (camera_fov, transform_cam_glbl2lcl, img_shape) = del_raycast::parse_pbrt::hoge(&scene);
     for shape_entity in scene.shapes {
         let transform = shape_entity.transform.to_cols_array();
-        let (tri2vtx, vtx2xyz) =
+        let (tri2vtx, vtx2xyz, _, _) =
             del_raycast::parse_pbrt::trimesh3_from_shape_entity(&shape_entity, path_file).unwrap();
         let bvhnodes = del_msh_core::bvhnodes_morton::from_triangle_mesh(&tri2vtx, &vtx2xyz, 3);
         let bvhnode2aabb = del_msh_core::aabbs3::from_uniform_mesh_with_bvh(
