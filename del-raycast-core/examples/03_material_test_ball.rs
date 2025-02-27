defbde10d5f038568653ae0e11e4c6073390648e
leo <lch01234@gmail.com>
2025-02-27 14:34:48 +0900

    test dielectric sphere

--- del-raycast-core/examples/03_material_test_ball.rs
+++ del-raycast-core/examples/03_material_test_ball.rs
@@ -130,6 +130,8 @@ fn parse() -> anyhow::Result<(
 }
 
 fn main() -> anyhow::Result<()> {
+    dielectric_sphere();
+
     let (shapes, camera, scene) = parse()?;
     {
         let mut tri2vtx: Vec<usize> = vec![];
@@ -266,4 +268,115 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn dielectric_sphere() {
+    use arrayref::array_mut_ref;
+    use del_geo_core::mat4_col_major;
+    use del_raycast_core::material;
+    let res_path = "target/test_dielectric.hdr";
+    let env_map_path = "asset/material-testball/textures/envmap.pfm";
+    let (tex_shape, tex_data) = {
+        match del_raycast_core::io_pfm::PFM::read_from(env_map_path) {
+            Ok(pfm) => ((pfm.w, pfm.h), pfm.data),
+            Err(msg) => {
+                panic!("{}", msg);
+            }
+        }
+    };
+
+    let camera_fov = 20.0;
+    // let transform_cam_lcl2glbl = del_geo_core::mat4_col_major::from_translate(&[0., 0., -5.]);
+    let transform_world2camlcl: [f32; 16] = [
+        0.721367, -0.373123, -0.583445, -0., -0., 0.842456, -0.538765, -0., -0.692553, -0.388647,
+        -0.60772, -0., 0.0258668, -0.29189, 5.43024, 1.,
+    ];
+    let transform_camlcl2world =
+        del_geo_core::mat4_col_major::try_inverse(&transform_world2camlcl).unwrap();
+    let transform_env = [
+        -0.386527, 0., 0.922278, 0., -0.922278, 0., -0.386527, 0., 0., 1., 0., 0., 0., 0., 0., 1.,
+    ];
+    let transform_env: [f32; 16] = {
+        let m = nalgebra::Matrix4::<f32>::from_column_slice(&transform_env);
+        let m = m.try_inverse().unwrap();
+        // let transform_env = del_geo_core::mat4_col_major::try_inverse(&transform_env).unwrap();
+        m.as_slice().try_into().unwrap()
+    };
+    let sphere_cntr = [0.15, 0.50, 0.16];
+    let ior = 1.5; // index of refraction
+    let img_shape = (640, 360);
+    {
+        let shoot_ray = |i_pix: usize, pix: &mut [f32]| {
+            let pix = array_mut_ref![pix, 0, 3];
+            let (ray_org, ray_dir) = del_raycast_core::cam_pbrt::cast_ray_plus_z(
+                (i_pix % img_shape.0, i_pix / img_shape.0),
+                (0., 0.),
+                img_shape,
+                camera_fov,
+                transform_camlcl2world,
+            );
+            let t = del_geo_core::sphere::intersection_ray(0.7, &sphere_cntr, &ray_org, &ray_dir);
+            if let Some(t) = t {
+                use del_geo_core::vec3;
+                let pos = vec3::axpy::<f32>(t, &ray_dir, &ray_org);
+                let nrm = vec3::sub(&pos, &sphere_cntr);
+                let hit_nrm = vec3::normalize(&nrm);
+
+                let dir_inc = {
+                    let local_nrm =
+                        mat4_col_major::transform_direction(&transform_world2camlcl, &hit_nrm);
+                    let local_dir =
+                        mat4_col_major::transform_homogeneous(&transform_world2camlcl, &ray_dir)
+                            .unwrap();
+                    if let Some((out, eta)) = material::refract(
+                        &vec3::normalize(&local_dir),
+                        &vec3::normalize(&local_nrm),
+                        1. / ior,
+                    ) {
+                        mat4_col_major::transform_direction(
+                            &transform_camlcl2world,
+                            &vec3::normalize(&out),
+                        )
+                    } else {
+                        let refl = vec3::mirror_reflection(&ray_dir, &hit_nrm);
+                        vec3::normalize(&refl)
+                    }
+                };
+
+                let env =
+                    del_geo_core::mat4_col_major::transform_homogeneous(&transform_env, &dir_inc)
+                        .unwrap();
+                let tex_coord = del_geo_core::uvec3::map_to_unit2_equal_area(&env);
+                *pix = del_canvas::texture::nearest_integer_center::<3>(
+                    &[
+                        tex_coord[0] * tex_shape.0 as f32,
+                        tex_coord[1] * tex_shape.1 as f32,
+                    ],
+                    &tex_shape,
+                    &tex_data,
+                );
+            } else {
+                let nrm = del_geo_core::vec3::normalize(&ray_dir);
+                let env = del_geo_core::mat4_col_major::transform_homogeneous(&transform_env, &nrm)
+                    .unwrap();
+                let tex_coord = del_geo_core::uvec3::map_to_unit2_equal_area(&env);
+                *pix = del_canvas::texture::nearest_integer_center::<3>(
+                    &[
+                        tex_coord[0] * tex_shape.0 as f32,
+                        tex_coord[1] * tex_shape.1 as f32,
+                    ],
+                    &tex_shape,
+                    &tex_data,
+                );
+            }
+        };
+        use rayon::iter::IndexedParallelIterator;
+        use rayon::iter::ParallelIterator;
+        use rayon::prelude::ParallelSliceMut;
+        let mut img = vec![0f32; img_shape.0 * img_shape.1 * 3];
+        img.par_chunks_mut(3)
+            .enumerate()
+            .for_each(|(i_pix, pix)| shoot_ray(i_pix, pix));
+        let _ = del_canvas::write_hdr_file(res_path, img_shape, &img);
+    }
 }
